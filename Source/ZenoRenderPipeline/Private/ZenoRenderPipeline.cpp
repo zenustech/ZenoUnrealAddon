8836aa092792e8664913dab9d8cbc71568e2aa8c
DarcJC <me@darc.pro>
2023-07-16 16:31:27 +0800

    faet: add render pipeline module

---
+++ Source/ZenoRenderPipeline/Private/ZenoRenderPipeline.cpp
@@ -0,0 +1,17 @@
+﻿#include "ZenoRenderPipeline.h"
+
+#define LOCTEXT_NAMESPACE "FZenoRenderPipelineModule"
+
+void FZenoRenderPipelineModule::StartupModule()
+{
+    
+}
+
+void FZenoRenderPipelineModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FZenoRenderPipelineModule, ZenoRenderPipeline)
